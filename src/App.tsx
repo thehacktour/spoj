9183abcbcd5e8ad43a52306f1b356798897ced19
Atilio <atilio.jesus@grupoboticario.com.br>
2024-10-27 20:28:34 -0300

    refactor: ♻︎ ajusta chamada da api

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
 
-  const [piada, setPiada] = useState("")
+  const [piada, setPiada] = useState({ setup: "", delivery: "" })
 
-  const gerarPiadaAleatoria = () => {
+  const gerarPiada = () => {
     const chamadaAPI = async () => {
-      const response = await fetch("https://v2.jokeapi.dev/joke/Any")
-      const data = await response.json()
-      if(data.setup){
-        setPiada(data.setup)
+      const chamada = await fetch("https://v2.jokeapi.dev/joke/Any")
+      const data = await chamada.json() 
+
+      if(data){
+        const setupOficial = data.setup
+        const deliveryOficial = data.delivery
+
+        setPiada((information) => ({ ...information, setup: setupOficial, delivery: deliveryOficial }))
       }else{
-        setPiada("Não foi encontrado uma piada!")
+        setPiada((information) => ({ ...information, setup: "Não foi encontrada uma piada", delivery: " " }))
       }
     }
+
     chamadaAPI()
   }
-
 
   return (
     <div>
       <h1>Gerador de Piadas Aleatorias</h1>
-      <button onClick={gerarPiadaAleatoria}>Gerar Piada</button>
-      <h1>Piada do dia: {piada}</h1>
+
+      <button onClick={gerarPiada}>Gerar Piadas</button>
+
+      <h1>Setup: {piada.setup}</h1>
+      <h1>Resposta: {piada.delivery}</h1>
     </div>
   );
 }
