4a222996d7f175b2ae9ba719c33b523165e08fdb
Atilio <atilio.jesus@grupoboticario.com.br>
2024-10-27 19:00:34 -0300

    feat: ✨ adiciona componente de gerador de piada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [piada, setPiada] = useState("")
+
+  const gerarPiadaAleatoria = () => {
+    const chamadaAPI = async () => {
+      const response = await fetch("https://v2.jokeapi.dev/joke/Any")
+      const data = await response.json()
+      if(data.setup){
+        setPiada(data.setup)
+      }else{
+        setPiada("Não foi encontrado uma piada!")
+      }
+    }
+    chamadaAPI()
+  }
+
+
   return (
     <div>
       <h1>Gerador de Piadas Aleatorias</h1>
+      <button onClick={gerarPiadaAleatoria}>Gerar Piada</button>
+      <h1>Piada do dia: {piada}</h1>
     </div>
   );
 }
